67f55ae2f382280d8032ba7502ce6825a240f367
laylabrassington <134307970+laylabrassington@users.noreply.github.com>
2025-10-09 14:16:47 -0500

    Update bseqsc_job_submission.sh

--- code for deconvultion/updated code 8oct25/bseqsc_job_submission.sh
+++ code for deconvultion/updated code 8oct25/bseqsc_job_submission.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --mail-user=your_emaill@university.edu
+#SBATCH --mail-user=your_email@vanderbilt.edu
 #SBATCH --mail-type=ALL
-#SBATCH -J bseqsc_job_date     	             
-#SBATCH --time=80:00:00              
+#SBATCH -J bseqsc_job_date
+#SBATCH --cpus-per-task=8
+#SBATCH --time=40:00:00              
 #SBATCH --mem=50GB
 #SBATCH -o bseqsc_job_date_out_%j.out     
 
 #reference script that has absolute path to the Rscript
 #need to submit job in directory where example_r_script.R and example_submission.sh is 
-/filepath/my_rscript.sh bseqsc_job_7oct25.R
+/filepath/my_rscript.sh bseqsc_job_date.R
